617a12bf9bbff759a334f349294fc2fb4b67b174
Proyecto CV <123872221+ivanmaiden@users.noreply.github.com>
2023-03-18 17:00:20 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+function vermas(id){
+if(id=="mas"){
+document.getElementById("personal_lista").style.display="block";   
+document.getElementById("mas").style.display="none"; 
+}
+else{
+document.getElementById("personal_lista").style.display="none";
+document.getElementById("mas").style.display="inline";
+}
+}
+
+function vermas1(id){
+if(id=="mas1"){
+document.getElementById("habilidades_lista").style.display="block";   
+document.getElementById("mas1").style.display="none"; 
+}
+else{
+document.getElementById("habilidades_lista").style.display="none";
+document.getElementById("mas1").style.display="inline";
+}
+}
+
+function vermas2(id){
+if(id=="mas2"){
+document.getElementById("experiencia_lista").style.display="block";   
+document.getElementById("mas2").style.display="none"; 
+}
+else{
+document.getElementById("experiencia_lista").style.display="none";
+document.getElementById("mas2").style.display="inline";
+}
+}
+function vermas3(id){
+if(id=="mas3"){
+document.getElementById("formulario_de_contacto").style.display="block";   
+document.getElementById("mas3").style.display="none"; 
+}
+else{
+document.getElementById("formulario_de_contacto").style.display="none";
+document.getElementById("mas3").style.display="inline";
+}
+}
